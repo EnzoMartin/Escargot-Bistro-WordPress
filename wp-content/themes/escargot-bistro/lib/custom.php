9a8d53fb5336e7bc4518a33192551eeaa5f5e2c5
Enzo Martin <enzo.r.martin@gmail.com>
2016-05-05 16:23:52 +0200

    Add menu post type - Remove post format from banners

--- wp-content/themes/escargot-bistro/lib/custom.php
+++ wp-content/themes/escargot-bistro/lib/custom.php
@@ -85,7 +85,7 @@ if ( ! function_exists('custom_banners') ) {
             'label'               => __( 'banner', 'text_domain' ),
             'description'         => __( 'A banner', 'text_domain' ),
             'labels'              => $labels,
-            'supports'            => array( 'title', 'revisions', 'post-formats', ),
+            'supports'            => array( 'title', 'revisions', ),
             'taxonomies'          => array( 'banner' ),
             'hierarchical'        => false,
             'public'              => true,
@@ -106,4 +106,49 @@ if ( ! function_exists('custom_banners') ) {
 
     // Hook into the 'init' action
     add_action( 'init', 'custom_banners', 0 );
+}
+
+// Register Menu Post Type
+if ( ! function_exists('custom_menu') ) {
+    function custom_menu() {
+        $labels = array(
+            'name'                => _x( 'Menus', 'Post Type General Name', 'text_domain' ),
+            'singular_name'       => _x( 'Menu', 'Post Type Singular Name', 'text_domain' ),
+            'menu_name'           => __( 'Menus', 'text_domain' ),
+            'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+            'all_items'           => __( 'All Menus', 'text_domain' ),
+            'view_item'           => __( 'View Menu', 'text_domain' ),
+            'add_new_item'        => __( 'Add New Menu', 'text_domain' ),
+            'add_new'             => __( 'Add New', 'text_domain' ),
+            'edit_item'           => __( 'Edit Menu', 'text_domain' ),
+            'update_item'         => __( 'Update Menu', 'text_domain' ),
+            'search_items'        => __( 'Search Menus', 'text_domain' ),
+            'not_found'           => __( 'Not found', 'text_domain' ),
+            'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+        );
+        $args = array(
+            'label'               => __( 'menu', 'text_domain' ),
+            'description'         => __( 'A menu', 'text_domain' ),
+            'labels'              => $labels,
+            'supports'            => array( 'title', 'editor', 'revisions', ),
+            'taxonomies'          => array( 'menu' ),
+            'hierarchical'        => false,
+            'public'              => true,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_nav_menus'   => true,
+            'show_in_admin_bar'   => true,
+            'menu_position'       => 5,
+            'menu_icon'           => 'dashicons-book-alt',
+            'can_export'          => true,
+            'has_archive'         => true,
+            'exclude_from_search' => true,
+            'publicly_queryable'  => false,
+            'capability_type'     => 'page',
+        );
+        register_post_type( 'menu', $args );
+    }
+
+    // Hook into the 'init' action
+    add_action( 'init', 'custom_menu', 0 );
 }
