c473b14464e9db1342b6cf63890e8ec4477a81a5
Enzo Martin <enzo.r.martin@gmail.com>
2016-05-07 00:15:00 +0200

    Expose menu post type

--- wp-content/themes/escargot-bistro/lib/custom.php
+++ wp-content/themes/escargot-bistro/lib/custom.php
@@ -153,7 +153,7 @@ if ( ! function_exists('custom_menu') ) {
             'labels'              => $labels,
             'supports'            => array( 'title', 'editor', 'revisions', ),
             'taxonomies'          => array( 'menu' ),
-            'hierarchical'        => false,
+            'hierarchical'        => true,
             'public'              => true,
             'show_ui'             => true,
             'show_in_menu'        => true,
@@ -162,9 +162,9 @@ if ( ! function_exists('custom_menu') ) {
             'menu_position'       => 12,
             'menu_icon'           => 'dashicons-book-alt',
             'can_export'          => true,
-            'has_archive'         => true,
-            'exclude_from_search' => true,
-            'publicly_queryable'  => false,
+            'has_archive'         => false,
+            'exclude_from_search' => false,
+            'publicly_queryable'  => true,
             'capability_type'     => 'page',
         );
         register_post_type( 'menu', $args );
