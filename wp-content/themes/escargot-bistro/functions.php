f25304e77dfe44491b03f410c3ad15a8106afcda
Enzo Martin <enzo.r.martin@gmail.com>
2016-05-08 23:17:04 +0200

    Remove jquery migrate

--- wp-content/themes/escargot-bistro/functions.php
+++ wp-content/themes/escargot-bistro/functions.php
@@ -287,4 +287,15 @@ function disable_wp_emojicons() {
 }
 add_action( 'init', 'disable_wp_emojicons' );
 
+add_filter( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+function remove_jquery_migrate( &$scripts)
+{
+    if(!is_admin())
+    {
+        $scripts->remove( 'jquery');
+        $scripts->add( 'jquery', false, array( 'jquery-core' ), '1.11.1' );
+    }
+}
+
 /* DON'T DELETE THIS CLOSING TAG */ ?>
