e3c7099ad946f4b605ab4832b6355a5f07909fa2
Enzo Martin <enzo.r.martin@gmail.com>
2016-05-16 01:19:23 +0200

    Small tweaks - Bust favico cache - Test out page scroll

--- wp-content/themes/escargot-bistro/header.php
+++ wp-content/themes/escargot-bistro/header.php
@@ -24,14 +24,14 @@ $mobile = $GLOBALS['is_mobile'] ? 'use-mobile' : 'use-desktop';
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png?v=1">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png?v=1">
 		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico?v=1">
 		<![endif]-->
 		<?php // or, set /favicon.ico for IE10 win ?>
 		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png?v=1">
         <meta name="theme-color" content="#00897b">
 
 		<?php // wordpress head functions ?>
