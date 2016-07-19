2a09cf7319c7427bc81966a408cf80da4449eac4
Enzo Martin <enzo.r.martin@gmail.com>
2016-07-19 10:31:58 +0200

    Move hours admin page to own file

--- wp-content/themes/escargot-bistro/functions.php
+++ wp-content/themes/escargot-bistro/functions.php
@@ -153,38 +153,7 @@ add_action( 'customize_register', 'bones_theme_customizer' );
 /************* ADMIN ****************/
 
 require_once( 'lib/help.php' );
-
-add_action( 'admin_menu', 'hours_menu' );
-
-function hours_menu() {
-	add_menu_page( 'Edit Hours of Operation', 'Hours', 'manage_options', 'hours-operation', 'hours_menu_options', 'dashicons-clock', 25 );
-}
-
-function hours_menu_options() {
-	$options = array('wpautop' => true, 'media_buttons' => false);
-	$message = '';
-
-	if(isset($_POST['hours_of_operation'])){
-		update_option('hours_of_operation',addslashes($_POST['hours_of_operation']));
-
-		$message = '<div id="message" class="updated notice notice-success is-dismissible"><p>Hours of operation updated.</p></div>';
-	}
-
-	?>
-	<div class="wrap">
-		<h1>Hours of Operation</h1>
-		<?php echo $message; ?>
-		<form method="post">
-            <?php wp_nonce_field('update-options') ?>
-			<?php wp_editor( get_option( 'hours_of_operation', '' ), 'hours_of_operation', $options ); ?>
-	        <p>
-		        <input type="submit" class="button button-primary button-large" name="Submit" value="Save" />
-	        </p>
-	        <input type="hidden" name="action" value="update" />
-	        <input type="hidden" name="page_options" value="hours_of_operation" />
-	    </form>
-	</div>
-<?php }
+require_once( 'lib/hours.php' );
 
 /************* ACTIVE SIDEBARS ********************/
 
---
+++ wp-content/themes/escargot-bistro/lib/hours.php
@@ -0,0 +1,32 @@
+<?php
+add_action( 'admin_menu', 'hours_menu' );
+
+function hours_menu() {
+	add_menu_page( 'Edit Hours of Operation', 'Hours', 'manage_options', 'hours-operation', 'hours_menu_options', 'dashicons-clock', 25 );
+}
+
+function hours_menu_options() {
+	$options = array('wpautop' => true, 'media_buttons' => false);
+	$message = '';
+
+	if(isset($_POST['hours_of_operation'])){
+		update_option('hours_of_operation',addslashes($_POST['hours_of_operation']));
+
+		$message = '<div id="message" class="updated notice notice-success is-dismissible"><p>Hours of operation updated.</p></div>';
+	}
+
+	?>
+	<div class="wrap">
+		<h1>Hours of Operation</h1>
+		<?php echo $message; ?>
+		<form method="post">
+            <?php wp_nonce_field('update-options') ?>
+			<?php wp_editor( get_option( 'hours_of_operation', '' ), 'hours_of_operation', $options ); ?>
+	        <p>
+		        <input type="submit" class="button button-primary button-large" name="Submit" value="Save" />
+	        </p>
+	        <input type="hidden" name="action" value="update" />
+	        <input type="hidden" name="page_options" value="hours_of_operation" />
+	    </form>
+	</div>
+<?php }
